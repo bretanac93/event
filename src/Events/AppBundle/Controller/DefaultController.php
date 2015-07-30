cb3f1fd43dfff1d9f1c515aa5b7cec801979d862
Cesar Bretana Gonzalez <bretanac@gmail.com>
2015-07-29 22:36:45 -0700

    Minor changes.

--- src/Events/AppBundle/Controller/DefaultController.php
+++ src/Events/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Events\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,14 @@ class DefaultController extends Controller
         $result = $this->get('app_utils')->sort($this->get('app_utils')->notificationsFor($this->getUser()));
 
         return $this->render('@App/Notification/index.html.twig', array('entities' => $result));
+    }
+
+    //TODO: Write the logic to delete a notification
+    //Description: I have to persist the user logged and the notification that the user wants to delete.
+    //TODO: After this, filter using this table, to show only non-deleted notifications to the user
+    //Description: Note that this 'deletion' mode, is logical only, not physical.
+    public function deleteNotificationsAction(Request $request)
+    {
+
     }
 }
