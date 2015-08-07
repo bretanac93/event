b10eae2911e1f92475f8c77334e16b2d53eb3d69
Jose J. Breijo <josejbreijo99@gmail.com>
2015-08-07 14:20:38 +0200

    working in done events

--- web/public/js/site.js
+++ web/public/js/site.js
@@ -1,5 +1,4 @@
 $(function () {
-
     $.extend($.expr[':'], {
         isBlock: function (obj) {
             if (!obj) return false;
@@ -17,36 +16,28 @@ $(function () {
         mobile = function mobilecheck() {
             var check = false;
             (function (a) {
-                if (/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))check = true
+                if (/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true
             })(navigator.userAgent || navigator.vendor || window.opera);
             return check;
         }();
-
     var eventType = mobile ? 'touchstart' : 'click';
 
     var morphComponent = (function () {
         var $profile = $('#profile'),
             $morphSearch = $('#morphsearch'),
             $ctrlClose = $('span.morphsearch-close'),
-
             isOpen = false,
-
             toggle = function (ev) {
                 if (ev) ev.preventDefault();
-
                 if (isOpen) $morphSearch.removeClass('open');
                 else $morphSearch.addClass('open');
-
                 isOpen = !isOpen;
             },
-
             locate = function () {
                 var offset = $window.scrollTop(),
                     container = $morphSearch[0].getBoundingClientRect(),
                     content = $('.morphsearch-content')[0].getBoundingClientRect();
-
-                if (!isOpen)
-                    $morphSearch.css('top', offset);
+                if (!isOpen) $morphSearch.css('top', offset);
                 else {
                     if (container.top > 10 || content.bottom < $window.height()) {
                         morphComponent.toggle();
@@ -81,20 +72,16 @@ $(function () {
     });
 
     // todo: it is used in input-group
-    //$('.date-picker').datepicker({
-    //    orientation: "right",
-    //    autoclose: true
-    //});
+    $('.date-picker').datepicker({
+        orientation: "right",
+        autoclose: true
+    });
 
     $window.scroll(function () {
         morphComponent.locate();
-
         var searchOffset = $('#searchMain')[0].getBoundingClientRect().top;
-
-        if (searchOffset < 40)
-            $('#searchTop').addClass('toggle');
-        else
-            $('#searchTop').removeClass('toggle');
+        if (searchOffset < 40) $('#searchTop').addClass('toggle');
+        else $('#searchTop').removeClass('toggle');
     });
 
     $('#searchTop').on(eventType, function (ev) {
@@ -107,17 +94,11 @@ $(function () {
     });
 
     function hideNavMobile(ev) {
-        if (hasParentClass(ev.target, 'navg'))
-            return;
-
-        var active = $('nav')
-            .find('div:not(.first):isBlock');
-
+        if (hasParentClass(ev.target, 'navg')) return;
+        var active = $('nav').find('div:not(.first):isBlock');
         if (active.length === 0) return;
-
         $(active[0]).fadeOut(100);
         $('.first').fadeIn(100);
-
         $(document).off(ev.type, hideNavMobile);
     }
 
@@ -127,7 +108,6 @@ $(function () {
         var $target = ['.', $this.attr('id').substr(3)].join('');
         $('.first').fadeOut(100);
         $($target).fadeIn(100);
-
         if (ev.type === 'touchstart') {
             $(document).on(ev.type, hideNavMobile);
         }
@@ -155,18 +135,22 @@ $(function () {
         var active = null,
             $that = undefined,
             show = function (obj) {
-                var $target = $that.parent().siblings(obj);
-
-                $target.addClass('active')
-                    .mouseleave(function () {
+                var $target = $that.parent().siblings(obj),
+                    hideTarget = function () {
                         hide($target);
                         reset();
-                    });
+                    };
+
+                $target.addClass('working');
+
+                mobile ?
+                    $('.event *:not(.working)').on('touchstart', hideTarget) :
+                    $target.mouseleave(hideTarget);
 
                 active = obj;
             },
             hide = function (obj) {
-                $(obj).removeClass('active');
+                $(obj).removeClass('working');
             },
             reset = function () {
                 if (active) {
@@ -174,11 +158,9 @@ $(function () {
                     active = null;
                 }
             };
-
         $('.lgroup').mouseenter(function () {
             $that = $(this);
             var targetClass = ['.', $that.data('toggle')].join('');
-
             if (active) {
                 if (active !== targetClass) {
                     hide(active);
@@ -188,9 +170,8 @@ $(function () {
                 show(targetClass);
             }
         });
-
-        return {reset: reset};
-
+        return {
+            reset: reset
+        };
     })();
-
 });
