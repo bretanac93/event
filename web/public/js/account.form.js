6ed9b44f5121634dbe5a3592f9b35571819b1b69
Cesar Bretana Gonzalez <bretanac@gmail.com>
2015-09-04 14:00:27 +0200

    Minor changes in frontend.

--- web/public/js/account.form.js
+++ web/public/js/account.form.js
@@ -1,44 +1,61 @@
-$(document).ready(function () {
-    $('#options').show();
-    $('#register-form').hide();
-    $('#login-form').hide();
+var socialButtons = $('.social-buttons'),
+    brand = $('.brand'),
+    registerForm = $('.register-form'),
+    loginForm = $('.login-form'),
+    registerFooter = $('.register-footer'),
+    loginFooter = $('.login-footer');
+
+
+$('a[href=#rlogin-modal]').click(function () {
+    socialButtons.show();
+    brand.show();
+    registerForm.hide();
+    loginForm.hide();
+    registerFooter.hide();
+    loginFooter.show();
 });
 $('#register-show').click(function (e) {
     e.preventDefault();
-    $('#options').hide();
-    $('#register-form').show();
-    $('#login-form').hide();
+    brand.hide();
+    socialButtons.hide();
+    registerForm.show();
+    loginForm.hide();
 });
 $('#login-show').click(function (e) {
     e.preventDefault();
-    $('#options').hide();
-    $('#login-form').show();
-    $('#register-form').hide();
+    brand.hide();
+    socialButtons.hide();
+    registerForm.show();
+    loginForm.hide();
 });
 $('#init-show').click(function (e) {
     e.preventDefault();
-    $('#options').show();
-    $('#login-form').hide();
-    $('#register-form').hide();
+    brand.show();
+    socialButtons.show();
+    loginForm.hide();
+    registerForm.hide();
 });
 
 $('#init-show-login').click(function (e) {
     e.preventDefault();
-    $('#options').show();
-    $('#login-form').hide();
-    $('#register-form').hide();
+    brand.show();
+    socialButtons.show();
+    loginForm.hide();
+    registerForm.hide();
 });
 
 $('.register').click(function (e) {
     e.preventDefault();
-    $('#options').hide();
-    $('#login-form').hide();
-    $('#register-form').show();
+    brand.hide();
+    socialButtons.hide();
+    loginForm.hide();
+    registerForm.show();
 });
 
 $('#login').click(function (e) {
     e.preventDefault();
-    $('#options').hide();
-    $('#login-form').show();
-    $('#register-form').hide();
+    brand.hide();
+    socialButtons.hide();
+    loginForm.show();
+    registerForm.hide();
 });
