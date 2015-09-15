f42a23868f45cff9dbc9bd50faebb7c2ab9f1fd3
Cesar Bretana Gonzalez <bretanac@gmail.com>
2015-09-15 17:47:46 +0200

    Login & Register forms fixed in modal.

--- web/public/js/account.form.js
+++ web/public/js/account.form.js
@@ -5,8 +5,12 @@ var socialButtons = $('.social-buttons'),
     registerFooter = $('.register-footer'),
     loginFooter = $('.login-footer');
 
+function showInit() {
 
-$('a[href=#rlogin-modal]').click(function () {
+}
+
+$('.back').click(function (e) {
+    e.preventDefault();
     socialButtons.show();
     brand.show();
     registerForm.hide();
@@ -14,19 +18,33 @@ $('a[href=#rlogin-modal]').click(function () {
     registerFooter.hide();
     loginFooter.show();
 });
-$('#register-show').click(function (e) {
+
+$('a[href=#rlogin-modal]').click(function (e) {
     e.preventDefault();
-    brand.hide();
-    socialButtons.hide();
-    registerForm.show();
+    socialButtons.show();
+    brand.show();
+    registerForm.hide();
     loginForm.hide();
+    registerFooter.hide();
+    loginFooter.show();
 });
-$('#login-show').click(function (e) {
+$('.register-show').click(function (e) {
     e.preventDefault();
     brand.hide();
     socialButtons.hide();
     registerForm.show();
     loginForm.hide();
+    registerFooter.hide();
+    loginFooter.show();
+});
+$('.login-show').click(function (e) {
+    e.preventDefault();
+    brand.hide();
+    socialButtons.hide();
+    registerForm.hide();
+    loginForm.show();
+    loginFooter.hide();
+    registerFooter.show();
 });
 $('#init-show').click(function (e) {
     e.preventDefault();
